c8a70caec7ee11da9f8138a60b9e8cdb69a727ac
moreno063 <antomo063@gmail.com>
2025-04-03 12:51:37 -0500

    Subiendo ejercicios terminados

--- Pygame/assets/backgrounds/base.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="base" tilewidth="16" tileheight="16" tilecount="1400" columns="50">
- <image source="base.png" width="809" height="450"/>
- <tile id="1263">
-  <properties>
-   <property name="1" value=""/>
-  </properties>
- </tile>
-</tileset>
